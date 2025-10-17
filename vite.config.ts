0bdf9e422676b66a307e7075fba479de3212898e
Em-driod <sheenleen2@gmail.com.com>
2025-10-17 15:47:00 +0100

    book appoi

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server:{
-    historyApiFallback:true
+    // historyApiFallback:true // <-- This line was removed to fix the error.
   }
 })
